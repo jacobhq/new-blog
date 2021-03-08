50eb8bd5fc8535d484f34b7a3e825ff934fb06bb
Jacob Marshall <29145479+jacobhq@users.noreply.github.com>
2021-03-08 11:45:04 +0000

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -3,9 +3,9 @@ export default {
   titleSuffix: ' – JacobHQ',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Blog</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next Site Builder
+        JacobHQ
       </span>
     </>
   ),
