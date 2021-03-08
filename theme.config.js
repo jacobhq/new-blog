65deddf87ae186e74e4502706e78ad32882914bd
Jacob Marshall <29145479+jacobhq@users.noreply.github.com>
2021-03-08 11:30:49 +0000

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,6 +1,6 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  repository: 'https://github.com/jacobhq/new-blog',
+  titleSuffix: ' – JacobHQ',
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
@@ -37,5 +37,5 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditOnGitHubLink: true,
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>
+  footerText: <>MIT {new Date().getFullYear()} © Jacob Marshall.</>
 }
