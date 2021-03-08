0193d53ec3341782ccd1f7f22a02d607845a2af4
Jacob Marshall <29145479+jacobhq@users.noreply.github.com>
2021-03-08 11:48:46 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import 'nextra-theme-blog/style.css'
 
 export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Component {...pageProps} />
+    <script src="https://utteranc.es/client.js"
+        repo="jacobhq/new-blog"
+        issue-term="pathname"
+        label="comment"
+        theme="github-light"
+        crossorigin="anonymous"
+        async>
+</script>
+  )
 }
